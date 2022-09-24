490c0ee5e8cc44fc6e74532cacb17196a1f7f34e
Dolorosus <>
2022-09-24 20:24:08 +0200

    .

--- btrfs-snapshot-rotation.sh
+++ btrfs-snapshot-rotation.sh
@@ -16,8 +16,8 @@ colors=${orgname%%${orgname##*/}}COLORS.sh
 [ -f ${colors} ] && . ${colors}
 
 usage() {
-    scriptname=${0##*/}
-cat <<EOF
+	scriptname=${0##*/}
+	cat <<EOF
 $scriptname: Take and rotate snapshots on a btrfs file system
   Usage:
   $scriptname source target snap_name [count] [-q]
@@ -36,13 +36,13 @@ Example for anacrontab:
 7             30      weekly_snap     /usr/local/bin/btrfs-snapshot / /.btrfs weekly 5
 @monthly      90      monthly_snap    /usr/local/bin/btrfs-snapshot / /.btrfs monthly 3
 EOF
-    exit
+	exit
 }
 
 doit() {
 
 	$1 >/dev/null
-	if [ $? -eq 0 ] && [ -z "$QUIET"] ; then
+	if [ $? -eq 0 ] && [ -z "$QUIET"]; then
 		echo ${TICK} "$1"
 	else
 		echo ${CROSS} "$1"
@@ -50,26 +50,26 @@ doit() {
 }
 
 # Basic argument checks:
-if [ -z "$COUNT" ] ; then
+if [ -z "$COUNT" ]; then
 	echo ${CROSS} "COUNT is not provided."
 	usage
 fi
 
-if [ ! -z "$7" ] ; then
+if [ ! -z "$7" ]; then
 	echo ${CROSS} "Too many options."
 	usage
 fi
 
-if [ -n "$QUIET" ] && [ "x$QUIET" != "x-q"  ] ; then
+if [ -n "$QUIET" ] && [ "x$QUIET" != "x-q" ]; then
 	echo ${CROSS} "Option 4 is either -q or empty. Given: \"$QUIET\""
 	usage
 fi
 
 # $max_snap is the highest number of snapshots that will be kept for $MARK.
-max_snap=$((COUNT -1))
+max_snap=$((COUNT - 1))
 
 # Clean up older snapshots:
-for i in $(find "$TARGET"|sort |grep @"${MARK}"\$|head -n -${max_snap}); do
+for i in $(find "$TARGET" | sort | grep @"${MARK}"\$ | head -n -${max_snap}); do
 	doit "btrfs subvolume delete $i"
 done
 doit "btrfs subvolume sync $TARGET"
@@ -77,4 +77,3 @@ doit "btrfs subvolume sync $TARGET"
 # Create new snapshot:
 doit "btrfs subvolume snapshot -r $SOURCE $TARGET/${SNAP}@${MARK}"
 doit "btrfs subvolume sync $TARGET "
-
