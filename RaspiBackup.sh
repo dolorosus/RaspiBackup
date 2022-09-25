8e5651e52e9eb5da21f109d765fca44672677c96
Dolorosus <>
2022-09-25 23:48:05 +0200

    .

--- RaspiBackup.sh
+++ RaspiBackup.sh
@@ -25,29 +25,26 @@
 #
 #
 
-
-
 BOOTSIZE=250
 
 # in case COLORS.sh is missing
-msgok () {
+msgok() {
     echo -e "${TICK} ${1}${NOATT}"
 }
-msg () {
+msg() {
     echo -e "${IDENT} ${1}${NOATT}"
 }
-msgwarn () {
+msgwarn() {
     echo -e "${WARN} ${1}${NOATT}"
 }
 # Echos an error string in red text and exit
-error () {
+error() {
     echo -e "${CROSS} ${1}${NOATT}" >&2
     exit 1
 }
 
-
 # Creates a sparse "${IMAGE}"  and attaches to ${LOOPBACK}
-do_create () {
+do_create() {
 
     BOOTSIZE=${BOOTSIZE:-250}
 
@@ -72,7 +69,7 @@ do_create () {
 
 }
 
-change_bootenv () {
+change_bootenv() {
 
     declare -a srcpartuuid
     declare -a dstpartuuid
@@ -90,8 +87,7 @@ change_bootenv () {
     # assuming we have two partitions (/boot and /)
     #
 
-    for ((p = 1; p <= 2; p++))
-    do
+    for ((p = 1; p <= 2; p++)); do
         srcpartuuid[${p}]=$(lsblk -n -o PARTUUID "${SDCARD}${SUFFIX}${p}") || {
             msg "Could not find PARTUUID of ${SDCARD}${SUFFIX}${p}"
             editmanual=true
@@ -107,12 +103,12 @@ change_bootenv () {
         #
         grep -q "PARTUUID=${srcpartuuid[${p}]}" $fstab_tmp && {
             msg "Changeing PARTUUID from ${srcpartuuid[${p}]} to ${dstpartuuid[${p}]} in $fstab_tmp"
-            sed -i "s/PARTUUID=${srcpartuuid[${p}]}/PARTUUID=${dstpartuuid[${p}]}/" $fstab_tmp||{
+            sed -i "s/PARTUUID=${srcpartuuid[${p}]}/PARTUUID=${dstpartuuid[${p}]}/" $fstab_tmp || {
                 msgwarn "PARTUUID ${dstpartuuid[2]} has not been changed in  $fstab_tmp"
                 editmanual=true
             }
 
-            }||{
+        } || {
             msgwarn "PARTUUID=${srcpartuuid[${p}]} not found in $fstab_tmp"
             editmanual=true
         }
@@ -120,8 +116,7 @@ change_bootenv () {
     #
     # Something went wrong automatically changing wasn't possible
     # Now the Uuser has a second chance
-    if ${editmanual}
-    then
+    if ${editmanual}; then
         msgwarn "fstab cannot be changed automatically."
         msgwarn "correct fstab on destination manually."
     else
@@ -140,17 +135,16 @@ change_bootenv () {
     }
     grep -q "PARTUUID=${srcpartuuid[2]}" $cmdline_tmp && {
         msg "Changeing PARTUUID from ${srcpartuuid[2]} to ${dstpartuuid[2]} in $cmdline_tmp"
-        sed -i "s/PARTUUID=${srcpartuuid[2]}/PARTUUID=${dstpartuuid[2]}/" $cmdline_tmp||{
+        sed -i "s/PARTUUID=${srcpartuuid[2]}/PARTUUID=${dstpartuuid[2]}/" $cmdline_tmp || {
             msgwarn "PARTUUID ${dstpartuuid[2]} has not been changed in $cmdline_tmp"
             editmanual=true
         }
-        }||{
+    } || {
         msgwarn "PARTUUID ${srcpartuuid[2]} not found in  $cmdline_tmp"
         editmanual=true
     }
 
-    if ${editmanual}
-    then
+    if ${editmanual}; then
         msgwarn "cmdline.txt cannot be changed automatically."
         msgwarn "correct cmdline.txt on destination manually."
     else
@@ -160,7 +154,7 @@ change_bootenv () {
 }
 
 # Mounts the ${IMAGE} to ${LOOPBACK} (if needed) and ${MOUNTDIR}
-do_mount () {
+do_mount() {
 
     # Check if do_create already attached the SD Image
     [ $(losetup -f) = ${LOOPBACK} ] && {
@@ -176,8 +170,7 @@ do_mount () {
     mount ${LOOPBACK}p1 ${MOUNTDIR}/boot
 }
 
-
-do_check () {
+do_check() {
 
     local err
 
@@ -207,9 +200,8 @@ do_check () {
     return ${err}
 }
 
-
 # Rsyncs content of ${SDCARD} to ${IMAGE} if properly mounted
-do_backup () {
+do_backup() {
 
     local rsyncopt
 
@@ -219,29 +211,29 @@ do_backup () {
     if mountpoint -q ${MOUNTDIR}; then
         msg "Starting rsync backup of / and /boot/ to ${MOUNTDIR}"
         msg "rsync /boot/ ${MOUNTDIR}/boot/"
-        rsync ${rsyncopt}  /boot/ ${MOUNTDIR}/boot/
+        rsync ${rsyncopt} /boot/ ${MOUNTDIR}/boot/
         msg ""
         msg "rsync / to ${MOUNTDIR}"
         rsync ${rsyncopt} --exclude='.gvfs/**' \
-        --exclude='tmp/**' \
-        --exclude='proc/**' \
-        --exclude='run/**' \
-        --exclude='sys/**' \
-        --exclude='mnt/**' \
-        --exclude='lost+found/**' \
-        --exclude='var/swap ' \
-        --exclude='home/*/.cache/**' \
-        --exclude='var/cache/apt/archives/**' \
-        --exclude='var/lib/docker/' \
-        --exclude='var/lib/containerd/' \
-        / ${MOUNTDIR}/
+            --exclude='tmp/**' \
+            --exclude='proc/**' \
+            --exclude='run/**' \
+            --exclude='sys/**' \
+            --exclude='mnt/**' \
+            --exclude='lost+found/**' \
+            --exclude='var/swap ' \
+            --exclude='home/*/.cache/**' \
+            --exclude='var/cache/apt/archives/**' \
+            --exclude='var/lib/docker/' \
+            --exclude='var/lib/containerd/' \
+            / ${MOUNTDIR}/
 
     else
         msg "Skipping rsync since ${MOUNTDIR} is not a mount point"
     fi
 }
 
-do_showdf () {
+do_showdf() {
 
     msg ""
     df -mh ${LOOPBACK}p1 ${LOOPBACK}p2
@@ -249,9 +241,10 @@ do_showdf () {
 }
 
 # Unmounts the ${IMAGE} from ${MOUNTDIR} and ${LOOPBACK}
-do_umount () {
+do_umount() {
     msg "Flushing to disk"
-    sync; sync
+    sync
+    sync
 
     msg "Unmounting ${LOOPBACK}p1 and ${LOOPBACK}p2 from ${MOUNTDIR}"
     umount ${MOUNTDIR}/boot
@@ -266,7 +259,7 @@ do_umount () {
 #
 # resize image
 #
-do_resize () {
+do_resize() {
     local SIZE=${1:-1000}
 
     do_check || error "Filesystemcheck failed. Resize aborted."
@@ -291,9 +284,9 @@ do_resize () {
 }
 
 # Compresses ${IMAGE} to ${IMAGE}.gz using a temp file during compression
-do_compress () {
+do_compress() {
     msg "Compressing ${IMAGE} to ${IMAGE}.gz"
-    pv -tpreb "${IMAGE}" | gzip > "${IMAGE}.gz.tmp"
+    pv -tpreb "${IMAGE}" | gzip >"${IMAGE}.gz.tmp"
     [ -s "${IMAGE}.gz.tmp" ] && {
         mv -f "${IMAGE}.gz.tmp" "${IMAGE}.gz"
         [ -n "${opt_delete}" ] && rm -f "${IMAGE}"
@@ -301,7 +294,7 @@ do_compress () {
 }
 
 # Tries to cleanup after Ctrl-C interrupt
-ctrl_c () {
+ctrl_c() {
     msg "Ctrl-C detected."
 
     if [ -s "${IMAGE}.gz.tmp" ]; then
@@ -316,8 +309,8 @@ ctrl_c () {
 }
 
 # Prints usage information
-usage () {
-cat<<EOF
+usage() {
+    cat <<EOF
     ${MYNAME}
 
     Usage:
@@ -399,24 +392,24 @@ fi
 #
 # Check for dependencies
 #
-for c in dd losetup parted sfdisk partx mkfs.vfat mkfs.ext4 mountpoint rsync
-do
+for c in dd losetup parted sfdisk partx mkfs.vfat mkfs.ext4 mountpoint rsync; do
     command -v ${c} >/dev/null 2>&1 || error "Required program ${c} is not installed"
 done
 
 # Read the command from command line
 case "${1}" in
 
-    start|mount|umount|check|gzip|chbootenv|showdf|resize) opt_command=${1}
+start | mount | umount | check | gzip | chbootenv | showdf | resize)
+    opt_command=${1}
     ;;
 
-
-    -h|--help)
-        usage
-        exit 0
+\
+    -h | --help)
+    usage
+    exit 0
     ;;
-    *)
-        error "Invalid command or option: ${1}\nSee '${MYNAME} --help for usage"
+*)
+    error "Invalid command or option: ${1}\nSee '${MYNAME} --help for usage"
     ;;
 esac
 shift 1
@@ -424,22 +417,26 @@ shift 1
 # Read the options from command line
 while getopts ":czdflL:i:r:s:" opt; do
     case ${opt} in
-        c)  opt_create=1;;
-        z)  opt_compress=1;;
-        d)  opt_delete=1;;
-        f)  opt_force=1;;
-        l)  opt_log=1;;
-        L)  opt_log=1
-        LOG=${OPTARG};;
-        i)  SDCARD=${OPTARG};;
-        r)  RSIZE=${OPTARG};;
-        s)  SIZE=${OPTARG}
-        BLOCKSIZE=1M ;;
-        \?) error "Invalid option: -${OPTARG}\nSee '${MYNAME} --help' for usage";;
-        :)  error "Option -${OPTARG} requires an argument\nSee '${MYNAME} --help' for usage";;
+    c) opt_create=1 ;;
+    z) opt_compress=1 ;;
+    d) opt_delete=1 ;;
+    f) opt_force=1 ;;
+    l) opt_log=1 ;;
+    L)
+        opt_log=1
+        LOG=${OPTARG}
+        ;;
+    i) SDCARD=${OPTARG} ;;
+    r) RSIZE=${OPTARG} ;;
+    s)
+        SIZE=${OPTARG}
+        BLOCKSIZE=1M
+        ;;
+    \?) error "Invalid option: -${OPTARG}\nSee '${MYNAME} --help' for usage" ;;
+    :) error "Option -${OPTARG} requires an argument\nSee '${MYNAME} --help' for usage" ;;
     esac
 done
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 #
 # setting defaults if -i or -s is ommitted
 #
@@ -449,10 +446,10 @@ BLOCKSIZE=${BLOCKSIZE:-$(blockdev --getss $SDCARD)}
 RSIZE=${RSIZE:-1000}
 
 case "${SDCARD}" in
-    "/dev/mmc"*) SUFFIX="p";;
-    "/dev/sd"*)  SUFFIX="";;
-    "/dev/disk/by-id/"*) SUFFIX="-part";;
-    *) SUFFIX="p";;
+"/dev/mmc"*) SUFFIX="p" ;;
+"/dev/sd"*) SUFFIX="" ;;
+"/dev/disk/by-id/"*) SUFFIX="-part" ;;
+*) SUFFIX="p" ;;
 esac
 
 # Preflight checks
@@ -476,8 +473,7 @@ fi
 # Checks for compressing the image
 #
 if [ -n "${opt_compress}" ] || [ ${opt_command} = gzip ]; then
-    for c in pv gzip
-    do
+    for c in pv gzip; do
         command -v ${c} >/dev/null 2>&1 || error "Required program ${c} is not installed"
     done
 
@@ -486,14 +482,13 @@ if [ -n "${opt_compress}" ] || [ ${opt_command} = gzip ]; then
     fi
 fi
 
-
 #
 # Identify which loopback device to use
 #
 LOOPBACK=$(losetup -j "${IMAGE}" | grep -o ^[^:]*)
 if [ ${opt_command} = umount ]; then
     [ -z ${LOOPBACK} ] && error "No /dev/loop<X> attached to ${IMAGE}"
-    elif [ ! -z ${LOOPBACK} ]; then
+elif [ ! -z ${LOOPBACK} ]; then
     error "${IMAGE} already attached to ${LOOPBACK} mounted on $(grep ${LOOPBACK}p2 /etc/mtab | cut -d ' ' -f 2)/"
 else
     LOOPBACK=$(losetup -f)
@@ -507,12 +502,12 @@ if [ -z ${MOUNTDIR} ]; then
     MOUNTDIR=/mnt/$(basename "${IMAGE}")/
 else
     opt_mountdir=1
-    [  -d ${MOUNTDIR} ] || error "Mount point ${MOUNTDIR} does not exist"
+    [ -d ${MOUNTDIR} ] || error "Mount point ${MOUNTDIR} does not exist"
 fi
 
 # Check if default mount point exists
 if [ ${opt_command} = umount ]; then
-    [  -d ${MOUNTDIR} ] || error "Default mount point ${MOUNTDIR} does not exist"
+    [ -d ${MOUNTDIR} ] || error "Default mount point ${MOUNTDIR} does not exist"
 else
     if [ ! -n "${opt_mountdir}" ] && [ -d ${MOUNTDIR} ]; then
         error "Default mount point ${MOUNTDIR} already exists"
@@ -528,58 +523,58 @@ fi
 # Trap keyboard interrupt (ctrl-c)
 trap ctrl_c SIGINT SIGTERM
 
-
 # Do the requested functionality
 case ${opt_command} in
-    start)
-        msg "Starting SD Image backup process"
-        if [ ! -f "${IMAGE}" ] && [ -n "${opt_create}" ]; then
-            do_create
-        fi
-        do_mount
-        do_backup
-        change_bootenv
-        do_showdf
-        do_umount
-        if [ -n "${opt_compress}" ]; then
-            do_compress
-        fi
-        msgok "SD Image backup process completed."
-        if [ -n "${opt_log}" ]; then
-            msg "See rsync log in ${LOG}"
-        fi
-    ;;
-    mount)
-        if [ ! -f "${IMAGE}" ] && [ -n "${opt_create}" ]; then
-            do_create
-        fi
-        do_mount
-        msgok "SD Image has been mounted and can be accessed at:\n    ${MOUNTDIR}"
-    ;;
-    umount)
-        do_umount
-    ;;
-    check)
-        do_check
-    ;;
-    gzip)
+start)
+    msg "Starting SD Image backup process"
+    if [ ! -f "${IMAGE}" ] && [ -n "${opt_create}" ]; then
+        do_create
+    fi
+    do_mount
+    do_backup
+    change_bootenv
+    do_showdf
+    do_umount
+    if [ -n "${opt_compress}" ]; then
         do_compress
+    fi
+    msgok "SD Image backup process completed."
+    if [ -n "${opt_log}" ]; then
+        msg "See rsync log in ${LOG}"
+    fi
+    ;;
+mount)
+    if [ ! -f "${IMAGE}" ] && [ -n "${opt_create}" ]; then
+        do_create
+    fi
+    do_mount
+    msgok "SD Image has been mounted and can be accessed at:\n    ${MOUNTDIR}"
+    ;;
+umount)
+    do_umount
+    ;;
+check)
+    do_check
+    ;;
+gzip)
+    do_compress
     ;;
 
-    chbootenv)
-        do_mount
-        change_bootenv
-        do_umount
+chbootenv)
+    do_mount
+    change_bootenv
+    do_umount
     ;;
-    showdf)
-        do_mount
-        do_showdf
-        do_umount
+showdf)
+    do_mount
+    do_showdf
+    do_umount
     ;;
-    resize)
-    do_resize $RSIZE;;
-    *)
-        error "Unknown command: ${opt_command}"
+resize)
+    do_resize $RSIZE
+    ;;
+*)
+    error "Unknown command: ${opt_command}"
     ;;
 esac
 
